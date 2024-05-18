dfea7918f08d1ae6379e45c1f953eacc744f4a6c
Nathan <nhnwong@gmail.com>
2024-05-18 19:46:28 -0400

    Update: Authentication redirection added
    Added authentication redirection when not logged in

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,34 @@
 import { Layout } from './Layout';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { useState } from 'react';
+import AuthRedirect from '../components/auth/AuthRedirect';
+const interests = ['JavaScript', 'CSS', 'HTML'];
 
 export const Profile = () => {
-  return <Layout>Profile</Layout>;
+  const [selectedInterests, setSelectedInterests] = useState([
+    false,
+    false,
+    false,
+  ]);
+
+  return (
+    <AuthRedirect>
+      <Layout>
+        <Stack direction="row" spacing={1}>
+          <div>
+            {interests.map((interest, i) => (
+              <Chip
+                label="Chip Filled"
+                onClick={() => {
+                  console.log(interest);
+                }}
+              />
+            ))}
+          </div>
+          <div>HELLO!</div>
+        </Stack>
+      </Layout>
+    </AuthRedirect>
+  );
 };
--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,48 +1,74 @@
 import { Layout } from './Layout';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, ConversationHeader } from '@chatscope/chat-ui-kit-react';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  ConversationHeader,
+} from '@chatscope/chat-ui-kit-react';
 import { useState } from 'react';
 import { TeamManagement } from '../components/TeamManagement.tsx';
+import AuthRedirect from '../components/auth/AuthRedirect';
 
 export const Team = () => {
   const [messages, setMessages] = useState<string[]>([]);
   const [viewTeam, changeStatus] = useState<boolean>(false);
   return (
-    <Layout>
-      <div className='size-full'>
-        <MainContainer>
-          <ChatContainer>
-            <ConversationHeader>
-              <ConversationHeader.Content>
-                <span
-                  style={{
-                    alignSelf: 'flex-center',
-                    color: '#ec1212'
-                  }}
-                > <button onClick={()=>{changeStatus(!viewTeam)}} className={'hover:opacity-50'}>
-                    Team Management
-                  </button>
-                </span>
-              </ConversationHeader.Content>
-            </ConversationHeader>
-            <MessageList>
-            {viewTeam ? <TeamManagement /> : 
-              <div>
-                {messages.map(message => 
-                  <Message model={{
-                    message: message,
-                    sentTime: "just now",
-                    sender: "Joe",
-                    direction: 'incoming',
-                    position: 'first'
-                    }} />
-                    )}
-              </div>}
-            </MessageList>    
-            <MessageInput attachButton={false} placeholder='Type Message' onSend={(innerHtml)=>setMessages([...messages, innerHtml])}/>
-          </ChatContainer>
-        </MainContainer>
-      </div>
-    </Layout>
-  )
-}
+    <AuthRedirect>
+      <Layout>
+        <div className="size-full">
+          <MainContainer>
+            <ChatContainer>
+              <ConversationHeader>
+                <ConversationHeader.Content>
+                  <span
+                    style={{
+                      alignSelf: 'flex-center',
+                      color: '#ec1212',
+                    }}
+                  >
+                    {' '}
+                    <button
+                      onClick={() => {
+                        changeStatus(!viewTeam);
+                      }}
+                      className={'hover:opacity-50'}
+                    >
+                      Team Management
+                    </button>
+                  </span>
+                </ConversationHeader.Content>
+              </ConversationHeader>
+              <MessageList>
+                {viewTeam ? (
+                  <TeamManagement />
+                ) : (
+                  <div>
+                    {messages.map((message) => (
+                      <Message
+                        model={{
+                          message: message,
+                          sentTime: 'just now',
+                          sender: 'Joe',
+                          direction: 'incoming',
+                          position: 'first',
+                        }}
+                      />
+                    ))}
+                  </div>
+                )}
+              </MessageList>
+              <MessageInput
+                attachButton={false}
+                placeholder="Type Message"
+                onSend={(innerHtml) => setMessages([...messages, innerHtml])}
+              />
+            </ChatContainer>
+          </MainContainer>
+        </div>
+      </Layout>
+    </AuthRedirect>
+  );
+};
