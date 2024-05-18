7b870d4b73e782815cc8ccdaf5af3bb14d5e5898
Nathan <nhnwong@gmail.com>
2024-05-18 16:30:55 -0400

    Update: Removed header
    header no longer needed

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,7 +5,7 @@ import { Footer } from '../components/Footer';
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col w-screen h-screen text-xl text-white">
-      <Header />
+      {/* <Header /> */}
       <div className="flex items-center justify-center bg-pink-200 grow">
         {children}
       </div>
