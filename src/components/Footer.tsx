1a8721d3ad11d3461a53172fc1b1874b603bdee3
Nathan <nhnwong@gmail.com>
2024-05-18 16:29:38 -0400

    Feat: Completed footer
    Footer links to react router elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
 import PersonIcon from '@mui/icons-material/Person';
 import GroupsIcon from '@mui/icons-material/Groups';
 import BlurOnIcon from '@mui/icons-material/BlurOn';
+import { Link } from "react-router-dom";
 export const Footer = () => {
 	return (
 		<div className="p-4 bg-pink-400 shadow-sm flex justify-evenly">
-			<GroupsIcon></GroupsIcon>
-			<BlurOnIcon></BlurOnIcon>
-			<PersonIcon></PersonIcon>
+			<Link to={'/team'}>
+				<GroupsIcon></GroupsIcon>
+			</Link>
+			<Link to={'/'}>
+				<BlurOnIcon></BlurOnIcon>
+			</Link>
+			<Link to={'/profile'}>
+				<PersonIcon></PersonIcon>
+			</Link>
 		</div>
 	)
 }
