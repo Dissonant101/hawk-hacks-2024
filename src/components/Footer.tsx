f0f07dd9d03d3785ebddbd22e67bbf0d20025f3c
Nathan <nhnwong@gmail.com>
2024-05-18 16:24:21 -0400

    Feat: Created footer icons
    Added footer icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,12 @@
+import PersonIcon from '@mui/icons-material/Person';
+import GroupsIcon from '@mui/icons-material/Groups';
+import BlurOnIcon from '@mui/icons-material/BlurOn';
 export const Footer = () => {
-	return <div className="p-4 bg-pink-400 shadow-sm flex justify-evenly">Footer</div>
+	return (
+		<div className="p-4 bg-pink-400 shadow-sm flex justify-evenly">
+			<GroupsIcon></GroupsIcon>
+			<BlurOnIcon></BlurOnIcon>
+			<PersonIcon></PersonIcon>
+		</div>
+	)
 }
