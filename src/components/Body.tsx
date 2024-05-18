c091c3cd171d9f94c40a32f53b0b94dc4775a288
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-05-18 11:36:20 -0400

    Push

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,3 +1,13 @@
+import { Card, TextField } from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
+
 export const Body = () => {
-	return <div className="grow bg-pink-200 flex justify-center items-center">Body</div>
-}
+  return (
+    <div className="grow bg-pink-200 flex justify-center items-center">
+      <Card>
+        <GoogleIcon />
+        <TextField />
+      </Card>
+    </div>
+  );
+};
