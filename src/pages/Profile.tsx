ebffb4d6cbb9a7f6be7a6a2e422ff08a08a539c9
Nathan <nhnwong@gmail.com>
2024-05-18 22:59:06 -0400

    Update: Created new UI design for Profile page
    - Created updated UI for Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -40,44 +40,53 @@ export const Profile = () => {
     <AuthRedirect>
       <Layout>
         <div className="flex flex-col gap-4 p-4">
-          <p>Programming Interests</p>
-          <div className="flex flex-wrap gap-2">
-            {selectedInterests.map((_, i) => (
-              <button
-                className={
-                  'px-2 rounded-full border border-pink-300 active:border-pink-500 active:bg-pink-800 ' +
-                  (selectedInterests[i] ? 'bg-pink-600' : '')
-                }
-                key={i}
-                onClick={() => {
-                  const newSelectedInterests = [...selectedInterests];
-                  newSelectedInterests[i] = !newSelectedInterests[i];
-                  setSelectedInterests(newSelectedInterests);
-                }}
-              >
-                {interests[i]}
-              </button>
-            ))}
+          <div className="bg-pink-600 p-4 rounded-xl">
+            <p className="bg-center flex justify-center">Create your profile</p>
           </div>
-          <Divider />
-          <p>Hackathons Attended</p>
-          <div className="flex flex-wrap gap-2">
-            {hackathons.map((_, i) => (
-              <button
-                className={
-                  'px-2 rounded-full border border-pink-300 active:border-pink-500 active:bg-pink-800 ' +
-                  (selectedHackathons[i] ? 'bg-pink-600' : '')
-                }
-                key={i}
-                onClick={() => {
-                  const newSelectedHackathons = [...selectedHackathons];
-                  newSelectedHackathons[i] = !newSelectedHackathons[i];
-                  setSelectedHackathons(newSelectedHackathons);
-                }}
-              >
-                {interests[i]}
-              </button>
-            ))}
+          <div className="bg-black p-8 rounded-xl">
+            <p className="text-white pt-0 pb-4">Programming Interests</p>
+            <div className="flex flex-wrap gap-2">
+              {selectedInterests.map((_, i) => (
+                <button
+                  className={
+                    'px-2 rounded-full border border-pink-300 active:border-pink-500 active:bg-pink-800 ' +
+                    (selectedInterests[i]
+                      ? 'bg-pink-600 text-white'
+                      : 'text-white')
+                  }
+                  key={i}
+                  onClick={() => {
+                    const newSelectedInterests = [...selectedInterests];
+                    newSelectedInterests[i] = !newSelectedInterests[i];
+                    setSelectedInterests(newSelectedInterests);
+                  }}
+                >
+                  {interests[i]}
+                </button>
+              ))}
+            </div>
+            <Divider />
+            <p className="text-white py-4">Hackathons Attended</p>
+            <div className="flex flex-wrap gap-2">
+              {hackathons.map((_, i) => (
+                <button
+                  className={
+                    'px-2 rounded-full border border-pink-300 active:border-pink-500 active:bg-pink-800 ' +
+                    (selectedHackathons[i]
+                      ? 'bg-pink-600 text-white'
+                      : 'text-white')
+                  }
+                  key={i}
+                  onClick={() => {
+                    const newSelectedHackathons = [...selectedHackathons];
+                    newSelectedHackathons[i] = !newSelectedHackathons[i];
+                    setSelectedHackathons(newSelectedHackathons);
+                  }}
+                >
+                  {hackathons[i]}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
       </Layout>
