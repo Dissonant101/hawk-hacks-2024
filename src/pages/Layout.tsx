72d8a80514ba1a881bd2f68a2eb42319b116ccf1
Nathan <nhnwong@gmail.com>
2024-05-18 20:20:17 -0400

    Feat: Created profile page and UI elements
    - Added Profile interests wall mapping interests and hackathons

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,7 +4,7 @@ import { Footer } from '../components/Footer';
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col w-screen h-screen text-xl text-white">
-      <div className="flex items-center justify-center bg-pink-200 grow">
+      <div className="flex flex-col items-center justify-center bg-pink-200 grow">
         {children}
       </div>
       <Footer />
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,30 +3,72 @@ import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import { useState } from 'react';
 import AuthRedirect from '../components/auth/AuthRedirect';
-const interests = ['JavaScript', 'CSS', 'HTML'];
+const interests = [
+  'JavaScript',
+  'CSS',
+  'HTML',
+  'C++',
+  'C#',
+  'Unity',
+  'Python',
+  'Java',
+  'React',
+  'Node.js',
+  'Express.js',
+  'AWS',
+];
+const hackathons = [
+  'HawkHacks',
+  'JAMHacks',
+  'Hack the North',
+  'OttawaHacks',
+  'StormHacks',
+  'ElleHacks',
+  'Cal Hacks',
+];
 
 export const Profile = () => {
-  const [selectedInterests, setSelectedInterests] = useState([
-    false,
-    false,
-    false,
-  ]);
+  const [selectedInterests, setSelectedInterests] = useState(
+    interests.map(() => false),
+  );
+  const [selectedHackathons, setSelectedHackathons] = useState(
+    hackathons.map(() => false),
+  );
 
   return (
     <AuthRedirect>
       <Layout>
-        <Stack direction="row" spacing={1}>
+        <Stack direction="row" spacing={3}>
           <div>
+            Programming Interests
             {interests.map((interest, i) => (
               <Chip
-                label="Chip Filled"
+                label={interest}
                 onClick={() => {
-                  console.log(interest);
+                  const newSelectedInterests = [...selectedInterests];
+                  // modify new
+                  newSelectedInterests[i] = !newSelectedInterests[i];
+                  setSelectedInterests(newSelectedInterests);
                 }}
               />
             ))}
           </div>
-          <div>HELLO!</div>
+        </Stack>
+        <Stack direction="row" spacing={3}>
+          <div>
+            Hackathons Attended
+            {hackathons.map((hackathon, i) => (
+              <Chip
+                label={hackathon}
+                onClick={() => {
+                  const newSelectedHackathons = [...selectedHackathons];
+                  // modify new
+                  newSelectedHackathons[i] = !newSelectedHackathons[i];
+                  setSelectedHackathons(newSelectedHackathons);
+                }}
+              />
+            ))}
+          </div>
         </Stack>
       </Layout>
     </AuthRedirect>
