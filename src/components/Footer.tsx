d2d27ab39cb56f198486b5576d953909c2aed98e
Jay <jay.jrjren@gmail.com>
2024-05-18 22:42:12 -0400

    Make Footer prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,22 @@
 import PersonIcon from '@mui/icons-material/Person';
 import GroupsIcon from '@mui/icons-material/Groups';
 import BlurOnIcon from '@mui/icons-material/BlurOn';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+const iconStyle = { fontSize: 48 };
+
 export const Footer = () => {
-	return (
-		<div className="p-10 bg-pink-400 shadow-sm flex justify-evenly">
-			<Link to={'/team'}>
-				<GroupsIcon className='hover:opacity-50' fontSize='large'></GroupsIcon>
-			</Link>
-			<Link to={'/'}>
-				<BlurOnIcon className='hover:opacity-50' fontSize='large'></BlurOnIcon>
-			</Link>
-			<Link to={'/profile'}>
-				<PersonIcon className='hover:opacity-50' fontSize='large'></PersonIcon>
-			</Link>
-		</div>
-	)
-}
+  return (
+    <div className="flex justify-between px-12 py-4 bg-pink-400 shadow-sm">
+      <Link to={'/team'}>
+        <GroupsIcon className="hover:opacity-50" style={iconStyle}></GroupsIcon>
+      </Link>
+      <Link to={'/'}>
+        <BlurOnIcon className="hover:opacity-50" style={iconStyle}></BlurOnIcon>
+      </Link>
+      <Link to={'/profile'}>
+        <PersonIcon className="hover:opacity-50" style={iconStyle}></PersonIcon>
+      </Link>
+    </div>
+  );
+};
