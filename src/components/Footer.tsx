74cf1acaf60178de6087b81c1a5dbac7f6df147f
Jay <jay.jrjren@gmail.com>
2024-05-19 00:29:34 -0400

    Update footer and swiper "like" ribbon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,21 @@ import GroupsIcon from '@mui/icons-material/Groups';
 import BlurOnIcon from '@mui/icons-material/BlurOn';
 import { Link } from 'react-router-dom';
 
-const iconStyle = { fontSize: 48 };
+const iconStyle = {
+  fontSize: 48,
+  '&:focus': {
+    fontSize: 56,
+  },
+};
 
 export const Footer = () => {
   return (
     <div className="flex justify-between px-12 py-4 bg-pink-400 shadow-sm">
       <Link to={'/team'}>
-        <GroupsIcon className="hover:opacity-50" style={iconStyle}></GroupsIcon>
+        <GroupsIcon
+          className="transition-all hover:opacity-50"
+          style={iconStyle}
+        ></GroupsIcon>
       </Link>
       <Link to={'/'}>
         <BlurOnIcon className="hover:opacity-50" style={iconStyle}></BlurOnIcon>
--- src/components/react-card-swiper/components/CardSwiperRibbons.tsx
+++ src/components/react-card-swiper/components/CardSwiperRibbons.tsx
@@ -1,32 +1,57 @@
 interface CardSwiperRibbonsProps {
   ribbonColors?: {
-    bgLike?: string
-    bgDislike?: string
-    textColor?: string
-  }
-  likeRibbonText?: string
-  dislikeRibbonText?: string
+    bgLike?: string;
+    bgDislike?: string;
+    textColor?: string;
+  };
+  likeRibbonText?: string;
+  dislikeRibbonText?: string;
 }
 
-function CardSwiperRibbons({ ribbonColors, dislikeRibbonText, likeRibbonText }: CardSwiperRibbonsProps) {
+function CardSwiperRibbons({
+  ribbonColors,
+  dislikeRibbonText,
+  likeRibbonText,
+}: CardSwiperRibbonsProps) {
   return (
-    <div className="swipe-card__ribbons-container" id="swipe-card__ribbons-container">
+    <div
+      className="swipe-card__ribbons-container"
+      id="swipe-card__ribbons-container"
+    >
       <div
         id="swipe-card__ribbon-like"
-        className="swipe-card__ribbon-like"
-        style={{ color: ribbonColors?.textColor, backgroundColor: ribbonColors?.bgLike }}
+        className="relative swipe-card__ribbon-like"
+        style={{
+          color: ribbonColors?.textColor,
+          // backgroundColor: ribbonColors?.bgLike,
+        }}
       >
-        {likeRibbonText || 'LIKE'}
+        {/* <img src="/heart.png" className="absolute scale-50 -translate-x-0" /> */}
+        <img
+          src="/heart.png"
+          className="absolute -translate-x-24 -translate-y-8 scale-[40%] -rotate-12"
+        />
+        <img
+          src="/heart.png"
+          className="absolute scale-[30%] -translate-x-2 -translate-y-20"
+        />
+        <img src="/heart.png" className="absolute scale-75" />
+        {/* <img src="/heart.png" className="absolute scale-50 -translate-x-0" />
+        <img src="/heart.png" className="absolute scale-50 -translate-x-0" /> */}
+        <span className="z-10">{likeRibbonText || 'LIKE'}</span>
       </div>
       <div
         id="swipe-card__ribbon-dislike"
         className="swipe-card__ribbon-dislike"
-        style={{ color: ribbonColors?.textColor, backgroundColor: ribbonColors?.bgDislike }}
+        style={{
+          color: ribbonColors?.textColor,
+          backgroundColor: ribbonColors?.bgDislike,
+        }}
       >
         {dislikeRibbonText || 'PASS'}
       </div>
     </div>
-  )
+  );
 }
 
-export default CardSwiperRibbons
+export default CardSwiperRibbons;
